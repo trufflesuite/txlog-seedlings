826f0b7b130c7cc7d3c54866463ba99ce4c335ac
Amal Sudama <cds.sudama@gmail.com>
2020-12-30 09:42:57 -0500

    Refactor test contracts

--- contracts/Bar.sol
+++ contracts/Bar.sol
@@ -5,35 +5,30 @@ import "./Spam.sol";
 
 contract Bar {
   Spam spam;
-  uint public lastValue;
 
   constructor(Spam _spam) { spam = _spam; }
 
-  function inside(uint x) pure internal returns (uint) {
+  function internal_inc(uint x) pure internal returns (uint) {
     return x + 1;
   }
 
   function inc(uint x) public returns (uint) {
-    lastValue = spam.double(x) ;
-    lastValue = inside(lastValue);
-    return lastValue;
+    return internal_inc(spam.double(x));
   }
 
   function incRevert(uint x) public returns (uint) {
-    lastValue = spam.doubleRevert(x);
-    lastValue = inside(lastValue);
-    return lastValue;
+    return internal_inc(spam.doubleRevert(x));
   }
 
   function tryAndCatchRevert(uint x) public returns(uint) {
+    uint rv;
     try spam.doubleRevert(x) returns (uint twice) {
-      lastValue = twice;
+      rv = twice;
     } catch Error(string memory /*someReason*/) {
-      lastValue = inside(lastValue + lastValue);
-    } catch(bytes memory /*crypticData*/) {
-      lastValue = inside(lastValue + lastValue);
+      rv = internal_inc(x + x - 1);
+    } catch(bytes memory /*someCrypticData*/) {
+      rv = internal_inc(x + x - 1);
     }
-    return lastValue;
+    return rv;
   }
-
 }
--- contracts/Foo.sol
+++ contracts/Foo.sol
@@ -3,28 +3,16 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "./Bar.sol";
 
-
 contract Foo {
   Bar bar;
 
   constructor(Bar _bar) { bar = _bar; }
 
-  function testBar(uint x) public returns(uint) {
-    return bar.inc(x);
-  }
+  function testBar(uint x) public returns(uint) { return bar.inc(x); }
 
-  function testBarTwice(uint x) public returns(uint) {
-    uint tmp = bar.inc(x);
-    return bar.inc(tmp);
-  }
+  function testBarTwice(uint x) public returns(uint) { return bar.inc(bar.inc(x)); }
 
-  function testBarTwiceRevert(uint x) public returns(uint) {
-    uint tmp = bar.incRevert(x);
-    return bar.incRevert(tmp);
-  }
+  function testBarTwiceRevert(uint x) public returns(uint) { return bar.incRevert(bar.incRevert(x)); }
 
-  function tryAndCatchRevert(uint x) public returns(uint) {
-    uint tmp = bar.tryAndCatchRevert(x);
-    return bar.inc(tmp);
-  }
+  function tryAndCatchRevert(uint x) public returns(uint) { return bar.inc(bar.tryAndCatchRevert(x)); }
 }
