a59f26754c7cf7edd7fb5551a9cef7922b6dc48c
Amal Sudama <cds.sudama@gmail.com>
2021-01-08 08:59:43 -0500

    limit to processing last 3 transactions to save time

--- gen-umls.zsh
+++ gen-umls.zsh
@@ -5,5 +5,5 @@
 TRUFFLE=~/work/truffle/packages/core/cli.js
 
 
-# execute `truffle run tx2seq --compile-tests TXN` for each transaction known to ganache
-node txns.js | grep -v done | xargs -n 1 node "${TRUFFLE}" run tx2seq --compile-tests > out
+# execute `truffle run tx2seq --compile-tests TXN` against the last 3 transactions on Ganache
+node txns.js | grep -v done | tail -3 | xargs -n 1 node "${TRUFFLE}" run tx2seq --compile-tests > out
