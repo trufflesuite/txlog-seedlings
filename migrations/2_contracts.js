07b69af0129db3a5dbe2844900ddb57099cfa288
Amal Sudama <cds.sudama@gmail.com>
2021-01-07 22:32:59 -0500

    zap unnecessary contracts

--- migrations/2_contracts.js
+++
@@ -1,9 +0,0 @@
-const Bar = artifacts.require("Bar");
-const Foo = artifacts.require("Foo");
-const Spam = artifacts.require("Spam");
-
-module.exports = async function(deployer) {
-  await deployer.deploy(Spam);
-  await deployer.deploy(Bar, Spam.address);
-  await deployer.deploy(Foo, Bar.address);
-};
