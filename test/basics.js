0728703c2b43d2b83b0e3bf45b21142f664ff5c8
Amal Sudama <cds.sudama@gmail.com>
2021-01-13 17:25:56 -0500

    Update clean room method
    The migration script makes sure the contracts are published at the
    start of `truffle test`. There is no need to create new instances
    at the beginning of the contract.

--- test/basics.js
+++ test/basics.js
@@ -5,19 +5,18 @@ const Second = artifacts.require("Second");
 contract("Entry", async function (accounts) {
   let instance;
   before(async () => {
-    const second = await Second.new();
-    const first = await First.new(second.address);
-    instance = await Entry.new(first.address);
+    instance = await Entry.deployed();
   });
+
+  it("tests a call twice: x -> 2(2x + 1) + 1", async () => {
+    const tx = await instance.test_a_call_twice(9);
+    assert.equal(await instance.value(), 11);
+  });
+
 
   it("tests a call: x -> 2x + 1", async () => {
     await instance.test_a_call(9);
     assert.equal(await instance.value(), 10);
-  });
-
-  it("tests a call twice: x -> 2(2x + 1) + 1", async () => {
-    await instance.test_a_call_twice(9);
-    assert.equal(await instance.value(), 11);
   });
 
   it("reverts an entire transaction", async () => {
