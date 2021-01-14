8572a46600d282606bb24d057c51cb91c84645ea
Amal Sudama <cds.sudama@gmail.com>
2021-01-14 15:42:13 -0500

    Migrate to use hardhat
     - update tests to use describe instead of contract
     - drop migration system and build contracts in `before` hooks
     - dependencies
     - use node 12 (set .nvmrc)

--- test/basics.js
+++ test/basics.js
@@ -2,35 +2,43 @@ const Entry = artifacts.require("Entry");
 const First = artifacts.require("First");
 const Second = artifacts.require("Second");
 
-contract("Entry", async function (accounts) {
+describe("Entry", async function() {
   let instance;
+  let accounts;
+
   before(async () => {
-    instance = await Entry.deployed();
+    const second = await Second.new();
+    const first = await First.new(second.address);
+    instance = await Entry.new(first.address);
   });
 
-  it("tests a call twice: x -> 2(2x + 1) + 1", async () => {
-    const tx = await instance.test_a_call_twice(9);
-    assert.equal(await instance.value(), 11);
-  });
+  describe("Basic tests", function() {
+
+    it("tests a call twice: x -> 2(2x + 1) + 1", async () => {
+      const tx = await instance.test_a_call_twice(9);
+      assert.equal(await instance.value(), 11);
+    });
 
 
-  it("tests a call: x -> 2x + 1", async () => {
-    await instance.test_a_call(9);
-    assert.equal(await instance.value(), 10);
-  });
+    it("tests a call: x -> 2x + 1", async () => {
+      await instance.test_a_call(9);
+      assert.equal(await instance.value(), 10);
+    });
 
-  it("reverts an entire transaction", async () => {
-    const originalValue = (await instance.value()).toNumber();
-    try {
-      await instance.test_a_revert(9);
-    } catch(err) {
-      assert.equal(true, true);
-    }
-    assert.equal(await instance.value(), originalValue);
-  });
+    it("reverts an entire transaction", async () => {
+      const originalValue = (await instance.value()).toNumber();
+      try {
+        await instance.test_a_revert(9);
+      } catch(err) {
+        assert.equal(true, true);
+      }
+      assert.equal(await instance.value(), originalValue);
+    });
 
-  it("catches a revert", async () => {
-    await instance.test_a_catch(3);
-    assert.equal(await instance.value(), 5);
+    it("catches a revert", async () => {
+      await instance.test_a_catch(3);
+      assert.equal(await instance.value(), 5);
+    });
+
   });
 });
--- test/insurance.js
+++ test/insurance.js
@@ -1,12 +1,15 @@
 const Insurance = artifacts.require("Insurance");
 const Beneficiary = artifacts.require("Beneficiary");
 
-contract("Insurance", async function ([owner, ...accounts]) {
+describe("Insurance", function () {
   let insurance;
   let beneficiaries;
+  let owner, accounts;
 
   before(async () => {
+    [owner, accounts] = await web3.eth.getAccounts();
     insurance = await Insurance.new();
+
     await web3.eth.sendTransaction({from: owner, to: insurance.address, value: 2e18});
 
     beneficiaries = [];
@@ -17,13 +20,14 @@ contract("Insurance", async function ([owner, ...accounts]) {
     }
   });
 
-  it("pays out", async () => {
-    const funds = 5000;
-    const tx = await insurance.payout(funds);
-    console.log(`truffle run tx2seq ${tx.tx}`);
-    const balances = await Promise.all(beneficiaries.map(b => web3.eth.getBalance(b.address))) ;
-    balances.forEach((b, i) => assert.equal(b, funds));
+  describe("Insurance tests", function() {
+
+    it("pays out", async () => {
+      const funds = 5000;
+      await insurance.payout(funds);
+      const balances = await Promise.all(beneficiaries.map(b => web3.eth.getBalance(b.address))) ;
+      balances.forEach((b, i) => assert.equal(b, funds));
+    });
+
   });
-
 });
-
