96ef0051b95531a87e3f705be654b211b8fdb4c2
Amal Sudama <cds.sudama@gmail.com>
2021-01-13 21:51:26 -0500

    simplify block walk

--- txns.js
+++ txns.js
@@ -1,20 +1,17 @@
 const Web3 = require('web3');
 const web3 = new Web3('http://localhost:8545');
 
-const latestBlockNumber = async () => {
-  const lb = await web3.eth.getBlock('latest');
-  return lb.number;
-}
-
 const blocks = async () => {
   let bin = 1;
-  let end = await latestBlockNumber();
-  console.log(`done: [${bin} - ${end}]`)
-
-  while (bin <= end) {
-    let block = await web3.eth.getBlock(bin++, true);
-    for (const tx of block.transactions) {
-      console.log(tx.hash);
+  while (true) {
+    try {
+      let block = await web3.eth.getBlock(bin++, true);
+      for (const tx of block.transactions) {
+        console.log(tx.hash);
+      }
+    } catch(e) {
+      // outta here
+      break
     }
   }
 }
