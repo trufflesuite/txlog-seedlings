9312948bc6e47899d136d0c7c1efcca6ac6e8e5c
Amal Sudama <cds.sudama@gmail.com>
2021-01-13 21:51:48 -0500

    compile-tests

--- gen-umls.zsh
+++ gen-umls.zsh
@@ -1,4 +1,4 @@
 #! /usr/bin/env zsh
 TRUFFLE=~/work/truffle/packages/core/cli.js
 # node txns.js| grep -v done | xargs -n 1 node $TRUFFLE run tx2seq --compile-tests --short-participant-names > out
-node txns.js| grep -v done | xargs -n 1 node $TRUFFLE run tx2seq > out
+node txns.js| grep -v done | xargs -n 1 node $TRUFFLE run tx2seq --compile-tests > out
