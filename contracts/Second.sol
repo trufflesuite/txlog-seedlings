619de2ed2ef09ecfdc66ef4fc074d611596f23e9
Amal Sudama <cds.sudama@gmail.com>
2021-02-18 11:47:44 -0500

    Make constructor payable

--- contracts/Second.sol
+++ contracts/Second.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Second {
 
+  constructor() payable {}
+
   function double_revert(uint x) public pure returns (uint) {
     require(false, 'Rats! Conditions are imperfect\nIm a bit sleepy...');
     return 2 * x;
