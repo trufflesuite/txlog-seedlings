2a371c2aae7379d2fe36d8da7437090e8810c672
Amal Sudama <cds.sudama@gmail.com>
2021-02-26 11:53:01 -0500

    Setup truffle-config for db/kit

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
     },
   },
 
@@ -15,10 +15,19 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.1",    // Fetch exact version from solc-bin (default: truffle's version)
-    }
+      version: "0.8.1", // Fetch exact version from solc-bin (default: truffle's version)
+    },
   },
   plugins: [
-    'txlog-to-plantuml'
-  ]
+    "txlog-to-plantuml",
+  ],
+  // db: {
+  //   enabled: true,
+  //   adapter: {
+  //     name: "sqlite",
+  //     settings: {
+  //       directory: ".db",
+  //     },
+  //   },
+  // },
 };
